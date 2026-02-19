16a9e2c9f0c87f7c72fc15f5f8b09dfe074bedc3
Arvin Amoukarami <arvinamoo2@gmail.com>
2026-02-18 23:29:38 -0500

    Initial Website Build

---
+++ main.js
@@ -0,0 +1,6 @@
+function scrollToQuote() {
+    const quoteSection = document.getElementById('quote-section');
+    quoteSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+console.log("Monkey Wrench Site Loaded Successfully!");
